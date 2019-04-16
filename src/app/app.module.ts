16b39972c34da0b765809136ba032a88ca4a52fb
Rachel Johnson <rach.johnson@hotmail.co.uk>
2019-04-16 21:14:15 +0100

    Add google auth (WIP)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { environment } from '../environments/environment';
+import { SuperSecretComponent } from './super-secret/super-secret.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuperSecretComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
